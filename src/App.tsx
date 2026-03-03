7f172d726fb681fd7ab4793f943d65764db62124
Lucas Farias <lucasfarias.ln@gmail.com>
2026-03-03 20:10:54 -0300

    Adiciona o componente ClientsScroll com suporte à exibição de clientes e lógica de carregamento

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { Clients } from './components/Clients';
-import { queryClient } from './lib/queryClient';
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { Clients } from "./components/Clients";
+import { queryClient } from "./lib/queryClient";
+import { ClientsScroll } from "./components/ClientsScroll";
 
 export function App() {
-
   return (
     <QueryClientProvider client={queryClient}>
       <div className="container py-10">
-        <Clients />
+        {/*   <Clients />*/}
+        <ClientsScroll />
       </div>
 
       <ReactQueryDevtools />
---
+++ src/components/ClientsScroll.tsx
@@ -0,0 +1,101 @@
+import { Skeleton } from "@/components/ui/Skeleton";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/Table";
+import { useClients } from "@/hooks/useClients";
+import { useEffect, useRef } from "react";
+
+export function ClientsScroll() {
+  const { clients, isLoading } = useClients();
+  const tableCaptionRef = useRef<null | HTMLTableCaptionElement>(null);
+
+  useEffect(() => {
+    if (!tableCaptionRef.current) return;
+    const observer = new IntersectionObserver((entries) => {
+      const { isIntersecting } = entries[0];
+      if (isIntersecting) {
+        console.log("Carregou mais clientes...");
+      }
+    });
+
+    observer.observe(tableCaptionRef.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [isLoading]);
+
+  return (
+    <div>
+      <header className="mb-6 pb-10">
+        <h1 className="text-3xl font-bold">Clientes</h1>
+      </header>
+
+      {isLoading && (
+        <div className="space-y-2">
+          <Skeleton className="w-full h-12" />
+          <Skeleton className="w-full h-12" />
+          <Skeleton className="w-full h-12" />
+          <Skeleton className="w-full h-12" />
+          <Skeleton className="w-full h-12" />
+          <Skeleton className="w-full h-12" />
+          <Skeleton className="w-full h-12" />
+          <Skeleton className="w-full h-12" />
+          <Skeleton className="w-full h-12" />
+        </div>
+      )}
+
+      {!isLoading && (
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Usuário</TableHead>
+              <TableHead>Data de entrada</TableHead>
+              <TableHead>Tipo de veículo</TableHead>
+              <TableHead>Marca</TableHead>
+              <TableHead>Modelo</TableHead>
+            </TableRow>
+          </TableHeader>
+
+          <TableBody>
+            {clients.map((client) => (
+              <TableRow key={client.id}>
+                <TableCell className="flex items-center gap-2">
+                  <img
+                    src={client.avatar}
+                    alt={client.name}
+                    className="w-10 h-10 rounded-full"
+                  />
+                  <div>
+                    <strong>{client.name}</strong>
+                    <small className="text-muted-foreground block">
+                      {client.email}
+                    </small>
+                  </div>
+                </TableCell>
+
+                <TableCell>{client.createdAt}</TableCell>
+
+                <TableCell>{client.vehicleType}</TableCell>
+
+                <TableCell>{client.vehicleManufacturer}</TableCell>
+
+                <TableCell>{client.vehicleModel}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+
+          <TableCaption ref={tableCaptionRef}>
+            <div className="w-10 h-10 bg-red-500"></div>
+          </TableCaption>
+        </Table>
+      )}
+    </div>
+  );
+}
